965e9abf22064488b9fccdda6e3679d893163a09
JoanK7 <j.kariza@alustudent.com>
2026-01-21 11:58:55 +0200

    view-caching

--- users/views.py
+++ users/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
 from users.models import User
 from users.serializers import UserSerializer
 
-
-# Create your views here.
-
-
+@method_decorator(cache_page(60 * 15), name='list')
+@method_decorator(cache_page(60 * 15), name='retrieve')
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
